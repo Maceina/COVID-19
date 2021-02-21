0f36c2f85334993e29681cfe987e1d419e70fd86
Maceina <maceinaaudrius@gmail.com>
2021-02-21 14:48:35 +0200

    server side is done

--- server/index.js
+++ server/index.js
@@ -1,25 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-const data = require('./data/data.json');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
+//const data = require('./data/data.json');
+const dataUrl ="https://opendata.ecdc.europa.eu/covid19/nationalcasedeath/json/";
 const app = express();
 const port = 5000 || process.env.port;
 app.use(cors());
 
-//Distinct country names
-const uniqueCountryNames = [...new Set(data.map(item => item.country))];
+//Read data from external point
+const getData = () => {
+  return axios.get(dataUrl).then((response) => {
+    response = response.data;
+    return response;
+  });
+};
 
-//Gets all data of a country passed as paramenter
-app.get('/:country', (req, res) => {
+//Fetched data from external point
+getData().then((data) => {
+  //Rest api endpoint, country passed as a parameter
+  app.get("/:country", (req,res)=>{
     const oneCountryData = data.filter(element => element.country === req.params.country);
-    //console.log(data);
     res.send(oneCountryData);
+  })
+  //Rest api endpoint, return all distinct country names
+  app.get("/",(req,res)=>{
+    const uniqueCountryNames = [...new Set(data.map(item => item.country))]
+    res.send(uniqueCountryNames);
+  })
 });
 
-//Gets all unique countries
-app.get('/', (req, res) => {
-  res.send(uniqueCountryNames);
-});
 
-
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
